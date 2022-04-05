fddcacaa4f49904dc90b9157c161e9cf28036cf3
dev081023 <yevhen081023@gmail.com>
2022-04-05 20:44:30 +0300

    add doctrine orm

---
+++ bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = [
+  'src/Models',
+];
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = array(
+  'driver' => 'pdo_mysql',
+  'host' => 'database',
+  'user' => getenv('DB_USERNAME_ROOT'),
+  'password' => getenv('DB_PASSWORD_ROOT'),
+  'dbname' => getenv('DB_DATABASE'),
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+global $entityManager;
+$entityManager = EntityManager::create($dbParams, $config);
+
+function getEntityManager(): EntityManager
+{
+  global $entityManager;
+  return $entityManager;
+}
