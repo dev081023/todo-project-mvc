1683ddfe0712a3d121f6eaa36beeaeea570504b5
dev081023 <yevhen081023@gmail.com>
2022-04-05 22:39:45 +0300

    add model User

---
+++ src/Models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Models;
+
+
+/**
+ * @Entity
+ * @Table(name="users")
+ */
+class User
+{
+  /**
+   * @Id
+   * @Column(name="id", type="integer")
+   * @GeneratedValue
+   */
+  private ?int $id;
+
+  /**
+   * @Column(name="login", type="string", length=150, nullable=false)
+   */
+  private string $login;
+
+  /**
+   * @Column(name="password", type="string", length=100, nullable=false)
+   */
+  private string $password;
+
+  public function __construct(string $login, string $password)
+  {
+    $this->login = $login;
+    $this->password = $password;
+  }
+
+  /**
+   * @return int|null
+   */
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getLogin(): string
+  {
+    return $this->login;
+  }
+
+  /**
+   * @return string
+   */
+  public function getPassword(): string
+  {
+    return $this->password;
+  }
+}
