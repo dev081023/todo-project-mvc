728c01c860dbff5d224961080e5794e665d44919
dev081023 <yevhen081023@gmail.com>
2022-04-05 21:07:50 +0300

    modified AbstractController

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -10,17 +10,16 @@ abstract class AbstractController
 
   public function viewWrapper(string $title, string $content): bool|string
   {
-    $time = time();
     ob_start();
-    require __DIR__ . '/../templates/html.php';;
+    require __DIR__ . '/../Views/default.php';;
     return ob_get_clean();
   }
 
   public function viewTemplate(string $template, array $variables = []): bool|string
   {
-    $template_file = __DIR__ . '/../templates/' . $template . '.html.php';
+    $template_file = __DIR__ . '/../Views/' . $template . '.html.php';
     if (!file_exists($template_file)) {
-      $template_file = './templates/not-found.html.php';
+      $template_file = './Views/not-found.html.php';
     }
     ob_start();
     require $template_file;
