472d329a5b1004423aa3021652efa6d32c47d050
dev081023 <yevhen081023@gmail.com>
2022-04-06 12:08:05 +0300

    modified: output tasks by id in reverse order

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -14,7 +14,8 @@ class TaskController extends AbstractController
     $vars = [];
     $em = getEntityManager();
     $taskRepository = $em->getRepository(Task::class);
-    $vars['tasks'] = $taskRepository->findAll();
+//    $vars['tasks'] = $taskRepository->findAll();
+    $vars['tasks'] = $taskRepository->findBy($vars, ['id' => 'DESC']);
     $content = $this->viewTemplate('tasks', $vars);
     $title = 'Tasks List';
     return $this->viewWrapper($title, $content);
